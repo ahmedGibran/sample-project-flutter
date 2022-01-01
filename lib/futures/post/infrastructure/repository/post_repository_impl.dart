3817f52e11f9cb86326c024b4c874644f2b21172
n <mac@MacBook-Pro-alkhas-b-Mac.local>
2022-01-01 11:02:37 +0100

    test post repository

--- lib/futures/post/infrastructure/repository/post_repository_impl.dart
+++ lib/futures/post/infrastructure/repository/post_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:tribe_app/core/errors/failure.dart';
+import 'package:tribe_app/core/errors/expceptions.dart';
+import 'package:tribe_app/core/network/network_info.dart';
 import 'package:tribe_app/futures/post/domain/entities/post.dart';
 import 'package:tribe_app/futures/post/domain/repository/post_repository.dart';
 import 'package:tribe_app/futures/post/infrastructure/data/post_remot_data.dart';
@@ -8,18 +9,25 @@ import 'package:tribe_app/futures/post/infrastructure/data/post_remot_data.dart'
 class PostRepositoryImpl extends PostRepository{
 
   final PostRemoteData postRemoteData;
+  final NetWorkInfo netWorkInfo;
   // final NetWorkInfo netWorkInfo;
-  PostRepositoryImpl({required this.postRemoteData, });
+  PostRepositoryImpl({required this.postRemoteData, required this.netWorkInfo});
 
   @override
   Future<Either<Failure,List<Post>>> getPost()async{
+   final isConnected = await  netWorkInfo.isConnected;
+   if(isConnected){
+     try{
+       List<Post> posts= await postRemoteData.getPosts();
+       return Right(posts);
+     }on ServerException{
+       return Left(Failure());
 
-    try{
-     List<Post> posts= await postRemoteData.getPosts();
-      return Right(posts);
-    }on ServerException{
-      return Left(Failure());
+     }
+   }else{
+     return Left(Failure());
 
-    }
+   }
+
   }
 }
---
+++ test/futures/post/infrastrucure/repository/post_repository_impl_test.dart
@@ -0,0 +1,55 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:tribe_app/core/errors/expceptions.dart' as ex;
+import 'package:tribe_app/core/errors/failure.dart';
+import 'package:tribe_app/core/network/network_info.dart';
+import 'package:tribe_app/futures/post/domain/domain.dart';
+import 'package:tribe_app/futures/post/domain/repository/post_repository.dart';
+import 'package:tribe_app/futures/post/infrastructure/data/post_remot_data.dart';
+import 'package:tribe_app/futures/post/infrastructure/repository/post_repository_impl.dart';
+
+import 'post_repository_impl_test.mocks.dart';
+@GenerateMocks([NetWorkInfo,PostRemoteData])
+void main(){
+  late MockNetWorkInfo mockNetWorkInfo;
+  late MockPostRemoteData mockPostRemoteData;
+  late PostRepository postRepository;
+
+  setUp((){
+    mockNetWorkInfo = MockNetWorkInfo();
+    mockPostRemoteData = MockPostRemoteData();
+    postRepository = PostRepositoryImpl(postRemoteData: mockPostRemoteData,
+        netWorkInfo:mockNetWorkInfo);
+  });
+
+  group("post repository there is connection",(){
+    List<Post> posts = [];
+    test("should call remote data when the internet is connected", ()async{
+      //
+      when(mockNetWorkInfo.isConnected).thenAnswer((_) async=> true);
+      when(mockPostRemoteData.getPosts()).thenAnswer((_) async=> posts);
+      //assr
+      final  result =await postRepository.getPost();
+      //act
+      expect(result,Right(posts));
+
+    });
+
+    test("should throw server exception when call remote data", ()async{
+      //
+      when(mockNetWorkInfo.isConnected).thenAnswer((_) async=> true);
+      when(mockPostRemoteData.getPosts()).thenAnswer((_) async=> throw ex.ServerException());
+      //assr
+      final result =await  postRepository.getPost();
+      verify(mockPostRemoteData.getPosts());
+      //act
+      expect(result.isLeft(),true);
+
+    });
+  });
+
+
+}
